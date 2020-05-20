c30444a2dcccd24faeae59cfcdde3f31242cf6bf
Swagat <swagat.miku@gmail.com>
2020-05-21 00:06:08 +0530

    test commit

--- JavaScript practice/js1.js
+++ JavaScript practice/js1.js
@@ -40,3 +40,65 @@ function fillCells() {
 for (var j of td) {
   j.addEventListener('click', fillCells);
 }
+
+
+
+
+// When calling a function on an event, always call with blank parameter and if you wish to pass any parameter pass it in an
+//intermediate function like below. And if you want to manipiulate some proporties of the same object use this keyword only inside the intermediate function.
+// Remember : This keyword don't work inside the last called function. Also this keyword won't work in the intermediate function if you pass the parameters.
+
+var p1 = 5;
+var p2 = 7;
+
+document.getElementById("myBtn").addEventListener("click", function() {
+  this.innerHTML = 'Clicked';
+  myFunction(p1, p2);
+});
+
+function myFunction(a, b) {
+  var result = a * b;
+  document.getElementById("demo").innerHTML = result;
+  
+}
+
+
+// The above is simillar to :
+
+var p1 = 5;
+var p2 = 7;
+
+document.getElementById("myBtn").addEventListener("click",myfun);
+
+function myfun() {
+  this.innerHTML = 'Clicked';
+  myFunction(p1, p2);
+}
+
+function myFunction(a, b) {
+  var result = a * b;
+  document.getElementById("demo").innerHTML = result;
+  
+}
+
+
+//Also if we want to call a function on an event when we can put the object which generated the event as a parameter , then we can do this in below way as well.
+
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction(x) {
+  x.style.background = "yellow";
+}
+</script>
+</head>
+<body>
+
+Enter your name: <input type="text" onfocus="myFunction(this)">
+
+<p>When the input field gets focus, a function is triggered which changes the background-color.</p>
+
+</body>
+</html>
+
